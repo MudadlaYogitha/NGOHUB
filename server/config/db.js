884cce3485a2a21e1a3ec6bb7e69e0aee44205e8
MudadlaYogitha <322103312128@gvpce.ac.in>
2025-06-21 12:15:29 +0530

    added all models

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const db = process.env.MONGO_URI;
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
-    console.log('MongoDB Connected...');
+    console.log('✅MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
--- server/server.js
+++ server/server.js
@@ -1,23 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+require('dotenv').config();
 
-dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-// Connect MongoDB Atlas
+// Connect to MongoDB
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+// Middleware
+app.use(express.json());
 
+// Routes
+app.use('/api/auth', require('./routes/api/auth'));
+
+// Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
